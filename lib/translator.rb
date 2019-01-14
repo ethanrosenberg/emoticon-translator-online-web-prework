9f1fffe0ca7ae115a9a444a51402a815b50f8569
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-01-14 05:55:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,25 +1,6 @@
 require "yaml"
   
-def load_library
-  # code goes here
-  
-  emoticons = YAML.load_file("lib/emoticons.yml")
-  
-   #emoticonindex = {}
-   emoticonindex = {'meaning'  => {}, 'emoticon' => {} }
 
-   emoticons.each do |key, value| 
-    eng = value[0]
-    jap = value[1]
-    
-    emoticonindex['emoticon'][eng] = jap
-    emoticonindex['meaning'][jap] = key
-    
-    
-  end
-  return emoticonindex
-  
-end
 
 def get_japanese_emoticon
   # code goes here
